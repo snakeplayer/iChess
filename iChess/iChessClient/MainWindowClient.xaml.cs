5a8b3b94097b0c39a2faeba1191cb1e3ec07f3a1
snakeplayer <chauche.benoit@gmail.com>
2018-05-03 16:26:11 +0200

    Client's login user interface

--- iChess/iChessClient/MainWindowClient.xaml.cs
+++ iChess/iChessClient/MainWindowClient.xaml.cs
@@ -1,4 +1,13 @@
-﻿using System;
+﻿/*
+ * Author: Benoit CHAUCHE
+ * Date: 2018
+ * Project: iChessClient
+ * Project description: A local network chess game. 
+ * File: MainWindowClient
+ * File description: The login user interface.
+ */
+
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -23,6 +32,10 @@ namespace iChessClient
         public MainWindow()
         {
             InitializeComponent();
+
+            // Custom icon
+            Uri iconUri = new Uri(@"C:\Users\Administrateur\Documents\T_DIPL\Documentation\Poster\Logo_iChess.png", UriKind.RelativeOrAbsolute);
+            this.Icon = BitmapFrame.Create(iconUri);
         }
     }
 }
